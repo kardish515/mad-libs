470bbbd8d95e06a88236ce086b92a8d931f5d8a1
Luke Stockburger and Nick Kardish <luke@>
2018-03-26 10:09:27 -0700

    add loop in javascript to make code clean

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,10 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    var occupationInput = $("input#occupation").val();
-    var femaleNameInput = $("input#female-name").val();
-    var locationInput= $("input#location").val();
-    var noun1Input = $("input#noun1").val();
-    var noun2Input = $("input#noun2").val();
-    var noun3Input = $("input#noun3").val();
-    var verbIngInput = $("input#verb-ing").val();
-    var pluralNounInput= $("input#plural-noun").val();
-    var verbIng2Input = $("input#verb-ing2").val();
-    var location2Input = $("input#location2").val();
-    var verbInput = $("input#verb").val();
-    $(".occupation").text(occupationInput);
-    $(".female-name").text(femaleNameInput);
-    $(".location").text(locationInput);
-    $(".noun1").text(noun1Input);
-    $(".noun2").text(noun2Input);
-    $(".noun3").text(noun3Input);
-    $(".verb-ing").text(verbIngInput);
-    $(".plural-noun").text(pluralNounInput);
-    $(".verb-ing2").text(verbIng2Input);
-    $(".location2").text(location2Input);
-    $(".verb").text(verbInput);
+    var blanks = ["occupation", "female-name", "location", "noun1", "noun2", "noun3", "verb-ing", "plural-noun", "verb-ing2", "location2", "verb"]
+    blanks.forEach(function(blank){
+      var userInput = $("input#" + blank).val();
+      $("." + blank).text(userInput);
+    });
     $("#story").show();
     event.preventDefault();
   });
